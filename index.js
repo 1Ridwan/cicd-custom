1e596b1455320884144aff85ce255959aad76a13
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-09-02 23:27:24 +0100

    bundle action with ncc

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const core = require('actions/core')
+import * as core from '@actions/core';
 
-try {
-    const nameToGreet = core.getInput('who-to-greet')
-    console.log(`Hello', ${nameToGreet}!`);
+async function run() {
+  try {
+    const nameToGreet = core.getInput('who-to-greet', { required: true });
+    console.log(`Hello, ${nameToGreet}!`);
     core.setOutput('time', new Date().toTimeString());
-} catch (error) {
-    core.setFailed(error.message);
+  } catch (err: unknown) {
+    const e = err as Error;
+    core.setFailed(e.message ?? String(err));
+  }
 }
+
+run();
